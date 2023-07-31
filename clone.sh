3b45aadc6b9893a44e3befc3f2b5d17421941e66
Kashif Razzaqui <kashifr@fb.com>
2023-07-31 15:37:01 +0100

    add github repos for checking out

--- clone.sh
+++ clone.sh
@@ -3,3 +3,5 @@
 echo "Cloning repositories...none."
 
 git clone git@github.com:marlonrichert/zsh-snap.git $HOME/code/zsh-snap
+gh repo clone kashifrazzaqui/t2k
+gh repo clone kashifrazzaqui/terraforming
