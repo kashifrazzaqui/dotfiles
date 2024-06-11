7a6fefbd088989c10e505df5b2f859db3db2a73e
Kashif Razzaqui <kashifr@fb.com>
2024-06-11 21:49:52 +0100

    fix key handling

--- exports.zsh
+++ exports.zsh
@@ -10,9 +10,6 @@ export SSH_KEY_PATH="~/.ssh/rsa_id"
 export WORKON_HOME=$HOME/.virtualenvs
 
 # added by setup_fb4a.sh
-#export ANDROID_SDK=/opt/android_sdk
-#export ANDROID_NDK_REPOSITORY=/opt/android_ndk
-#export ANDROID_HOME=${ANDROID_SDK}
 export JAVA_11=/usr/local/java-runtime/11
 export JAVA_8=/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home
 export JAVA_HOME=$JAVA_8
@@ -24,4 +21,4 @@ export LDFLAGS="-L/Users/kashifr/homebrew/lib"
 export CPPFLAGS="-I/Users/kashifr/homebrew/include"
 export PUBLIC_IP=$(curl --silent ipaddr.io)
 export TOGGLE_LAST_DIR
-export OPENAI_API_KEY=$(cat openapi.key)
+export OPENAI_API_KEY=$(cat /Users/kashifr/code/dotfiles/openapi.key)
